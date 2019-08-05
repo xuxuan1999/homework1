25f6c0df9e29272f1fa63d43115134cf6c1289ee
“xuxuan1999” <xu_xuan1999@sina.com>
2019-08-05 18:39:15 +0800

    first commit

---
+++ app.py
@@ -0,0 +1,105 @@
+from flask import Flask, render_template, flash, redirect, url_for, session, request, logging
+from data import Articles
+from flask_mysqldb import MySQL
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from passlib.hash import sha256_crypt
+
+app = Flask(__name__)
+
+# Config MySQL
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER']='root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'myflaskapp'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+# init MYSQL
+mysql=MySQL(app)
+
+Articles=Articles()
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/articles')
+def articles():
+    return render_template('articles.html',articles=Articles)
+
+@app.route('/article/<string:id>/')
+def article(id):
+    return render_template('article.html',id=id)
+
+class RegisterForm(Form):
+    name = StringField('Name', [validators.Length(min=1, max=50)])
+    username = StringField('Username', [validators.Length(min=4, max=25)])
+    email = StringField('Email', [validators.Length(min=6, max=50)])
+    password = PasswordField('Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords do not match')
+    ])
+    confirm = PasswordField('Confirm Password')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm(request.form)
+    if request.method == 'POST' and form.validate():
+        name = form.name.data
+        email = form.email.data
+        username = form.username.data
+        password = sha256_crypt.encrypt(str(form.password.data))
+        
+        # Create cursor
+        cur = mysql.connection.cursor()
+
+        # Execute qury
+        cur.execute("INSERT INTO users(name, email, username, password) VALUES(%s, %s, %s, %s)", (name, email, username, password))
+
+        # Commit to DB
+        mysql.connection.commit()
+
+        # Close connection
+        cur.close()
+
+        flash('You are now registered and can log in', 'success')
+        
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)
+
+# User login
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        # Get Form Fields
+        username = request.form['username']
+        password_candidate = request.form['password']
+
+        # Create cursor
+        cur = mysql.connection.cursor()
+
+        # Get user by username
+        result = cur.execute("SELECT * FROM users WHERE username = %s", [username])
+
+        if result > 0:
+            # GET stored hash
+            data = cur.fetchone()
+            password = data['password']
+
+            # Compare Password
+            if sha256_crypt.verify(password_candidate, password):
+                app.logger.info('PASSWORD MATCHED')
+            else:
+                error = 'Invalid login'
+                return render_template('login.html', error=error)
+        else:
+            error = 'Username not found'
+            return render_template('login.html',error=error)
+        
+    return render_template('login.html')
+
+if __name__ == '__main__':
+    app.secret_key='secret123'
+    app.run(debug=True)
---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id':1,
+            'title':'Article One',
+            'body':'Homework_Homework_Homework_Homework_Homework_Homework_Homework_HomeworkHomework_Homework_Homework_Homework_Homework',
+            'author':'xu xuan',
+            'create_date':'2019-8-1'
+        },
+        {
+            'id':2,
+            'title':'Article TWO',
+            'body':'Homework2_Homework2_Homework2_Homework2_Homework2_Homework2_Homework2_HomeworkHomework2_Homework2_Homework2_Homework2_Homework2',
+            'author':'xu xuan2',
+            'create_date':'2019-8-2'
+        },
+        {
+            'id':3,
+            'title':'Article THREE',
+            'body':'Homework3_Homework3_Homework3_Homework3_Homework3_Homework3_Homework3_HomeworkHomework3_Homework3_Homework3_Homework3_Homework3',
+            'author':'xu xuan3',
+            'create_date':'2019-8-3'
+        }
+    ]
+    return articles
